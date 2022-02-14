720af6eec572a533a491b948aba22e144a4ff571
John Deamers <john.deamers.alf@gmail.com>
2022-02-14 17:49:56 +0100

    fix pinia

--- frontend/app/src/main.ts
+++ frontend/app/src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import './assets/css/tailwind.css';
 
 const app = createApp(App).use(router);
+const pinia = createPinia();
+app.use(pinia);
 app.mount('#app');
