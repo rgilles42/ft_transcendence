5bd86cabad9e78e26a3ec905fdf30c465c999f8e
John Deamers <john.deamers.alf@gmail.com>
2022-02-14 02:12:57 +0100

    add swagger for backend documentation

--- backend/app/src/main.ts
+++ backend/app/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: 'http://localhost:8080', credentials: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Backend API Docs')
+    .setDescription('The backend of ft_transcendence')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
