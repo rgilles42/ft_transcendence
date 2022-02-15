93bf62c19f8b2dd77a42ff170649056088c59aa5
Raphaël Gilles <rgilles@student.42.fr>
2022-02-15 16:40:25 +0100

    now fetches users from actual db

--- backend/app/src/users/users.service.ts
+++ backend/app/src/users/users.service.ts
@@ -1,29 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserModel } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(UserModel) private usersRepository: Repository<UserModel>,
+  ) {}
+
+  findAll(): Promise<UserModel[]> {
+    return this.usersRepository.find();
   }
 
-  findAll() {
-    const users: number[] = [];
-    users.push(1);
-    users.push(5);
-    return users;
+  async findOne(id: number): Promise<UserModel> {
+    try {
+      const user = await this.usersRepository.findOneOrFail(id);
+      return user;
+    } catch (err) {
+      //OOPSIE
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+  // create(createUserDto: CreateUserDto): Promise<UserModel> {
+  //   const newUser = this.usersRepository.create()
+  // }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
 }
