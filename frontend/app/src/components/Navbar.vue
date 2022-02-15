ac9f6723430fe22e5d86c5d577bf55dde3536601
John Deamers <john.deamers.alf@gmail.com>
2022-02-16 00:40:20 +0100

    better navbar + sidebar

--- frontend/app/src/components/Navbar.vue
+++ frontend/app/src/components/Navbar.vue
@@ -1,7 +1,19 @@
 <template>
+<div>
   <header class="fixed top-0 w-full flex bg-gray-900" style="height: 50px;">
-    <div class="hidden lg:flex w-full mx-6 my-3">
+    <!-- Computer -->
+    <div class="hidden md:flex w-full mx-6 my-3">
       <div class="w-1/3 mr-auto flex flex-row nav-links">
+<!--
+        <button class="text-white relative ml-10 mr-10 w-6 focus:outline-none" @click="toggleSidebar()">
+          <span class="sr-only">Open main menu</span>
+          <div class="block w-5 absolute left-1/2 top-1/2   transform  -translate-x-1/2 -translate-y-1/2">
+            <span aria-hidden="true" class="block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out" :class="{'rotate-45': sidebarOpen,' -translate-y-1.5': !sidebarOpen }"></span>
+            <span aria-hidden="true" class="block absolute  h-0.5 w-5 bg-current transform transition duration-500 ease-in-out" :class="{'opacity-0': sidebarOpen } "></span>
+            <span aria-hidden="true" class="block absolute  h-0.5 w-5 bg-current transform  transition duration-500 ease-in-out" :class="{'-rotate-45': sidebarOpen, ' translate-y-1.5': !sidebarOpen}"></span>
+          </div>
+        </button> -->
+
         <router-link to="/" class="nav-link">Accueil</router-link>
       </div>
 
@@ -12,40 +24,56 @@
         <NavbarDropdownUser v-else :user="currentUser" />
       </div>
     </div>
-    <!-- Tablet -->
-    <div class="hidden lg:hidden md:flex container mx-auto my-3 justify-center">
-      <div class="w-1/3 mr-auto flex flex-row nav-links">
-        <router-link to="/" class="nav-link">Accueil</router-link>
-      </div>
 
-      <div class="w-1/3 ml-auto flex flex-row-reverse nav-links">
-        <template v-if="!currentUser">
-          <router-link to="/login" class="nav-link">Connexion</router-link>
-        </template>
-        <NavbarDropdownUser v-else :user="currentUser" />
-      </div>
-    </div>
     <!-- Mobile -->
-    <div class="md:hidden flex container mx-auto justify-center">
-      <router-link class="mt-3" to="/">
-      </router-link>
+    <div class="md:hidden container mx-auto flex w-full mx-6 my-3">
+      <div class="w-1/3 mr-auto flex flex-row nav-links">
+
+        <button class="text-white relative ml-10 mr-10 w-6 focus:outline-none" @click="toggleSidebar()">
+          <span class="sr-only">Open main menu</span>
+          <div class="block w-5 absolute left-1/2 top-1/2   transform  -translate-x-1/2 -translate-y-1/2">
+            <span aria-hidden="true" class="block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out" :class="{'rotate-45': sidebarOpen,' -translate-y-1.5': !sidebarOpen }"></span>
+            <span aria-hidden="true" class="block absolute  h-0.5 w-5 bg-current transform transition duration-500 ease-in-out" :class="{'opacity-0': sidebarOpen } "></span>
+            <span aria-hidden="true" class="block absolute  h-0.5 w-5 bg-current transform  transition duration-500 ease-in-out" :class="{'-rotate-45': sidebarOpen, ' translate-y-1.5': !sidebarOpen}"></span>
+          </div>
+        </button>
+      </div>
+
+      <div class="w-1/3 ml-auto flex flex-row-reverse nav-links">
+        <template v-if="!currentUser">
+          <router-link to="/login" class="nav-link">Connexion</router-link>
+        </template>
+        <NavbarDropdownUser v-else :user="currentUser" />
+      </div>
     </div>
+
   </header>
+
+  <Sidebar v-model="sidebarOpen" style="padding-top: 50px;" />
+
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
+import { defineComponent, ref, computed } from 'vue';
 import { useStore } from '@/store';
 import NavbarDropdownUser from './NavbarDropdownUser.vue';
+import Sidebar from './Sidebar.vue';
 
 export default defineComponent({
   name: 'Navbar',
-  components: { NavbarDropdownUser },
+  components: { NavbarDropdownUser, Sidebar },
   setup() {
     const store = useStore();
     return {
       currentUser: computed(() => store.getUser),
+      sidebarOpen: ref(false),
     };
+  },
+  methods: {
+    toggleSidebar() {
+      this.sidebarOpen = !this.sidebarOpen;
+    },
   },
 });
 </script>
---
+++ frontend/app/src/components/Sidebar.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="block md:hidden">
+    <header class="fixed h-full transform transition-transform duration-200 ease-in-out" :class="[(sidebarOpen ? 'translate-x-0' : '-translate-x-full')]">
+
+        <!-- Sidebar -->
+        <div id="sidebar" ref="sidebar" :class="[(sidebarOpen ? 'translate-x-0' : '-translate-x-full')]" class="transform transition-transform absolute z-40 left-0 top-0 static left-auto top-auto h-screen overflow-y-auto overflow-y-auto no-scrollbar w-72 flex-shrink-0 bg-gray-800 pt-4 pb-24">
+          <!-- Sidebar Header -->
+          <!-- <div class="flex justify-center pr-3 sm:px-2">
+
+          </div> -->
+
+              <!-- Links -->
+              <div>
+
+                <ul class="mt-3">
+
+                  <li class="py-1 rounded-sm mb-0.5 last:mb-0">
+                    <router-link
+                      to="/"
+                      class="nav-link m-0 block px-4 py-2 text-sm leading-5 transition duration-150 ease-in-out hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+                      role="menuitem"
+                    >
+                      <div class="flex flex-grow">
+                        <span class="flex-shrink-0 h-6 w-6 mr-3">
+                          <i class="fas fa-undo ml-1"></i>
+                        </span>
+                        <span class="text-sm font-medium">Accueil</span>
+                      </div>
+                    </router-link >
+                  </li>
+
+                </ul>
+
+              </div>
+
+        </div>
+      </header>
+      <transition name="fade">
+        <div v-if="sidebarOpen" id="sidebar-overlay" @click="sidebarOpen = false" class="fixed inset-0"></div>
+      </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue';
+
+export default defineComponent({
+  name: 'Sidebar',
+  props: {
+    modelValue: Boolean,
+  },
+  setup(props, { emit }) {
+    const sidebarStatus = ref(props.modelValue);
+    const sidebarOpen = computed({
+      get: () => sidebarStatus.value,
+      set: (value) => {
+        emit('update:modelValue', value);
+        sidebarStatus.value = value;
+      },
+    });
+    return {
+      sidebarOpen,
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.sidebarOpen = !this.sidebarOpen;
+    },
+  },
+  watch: {
+    modelValue: {
+      handler() {
+        this.sidebarOpen = this.modelValue;
+      },
+    },
+  },
+});
+</script>
+
+<style scoped>
+  header {
+    z-index: 999999;
+  }
+  #sidebar-overlay {
+    z-index: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .2s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
+
+</style>
