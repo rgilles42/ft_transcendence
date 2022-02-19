ed7827fa7529929a75884547b7a9ac7e681cf66f
ppaglier <ppaglier@student.42.fr>
2022-02-20 00:33:29 +0100

    remove global ChatBox

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app" class="flex flex-col min-h-screen">
     <Navbar />
-    <ChatBox />
     <div class="flex-grow" style="margin-top: 50px;">
       <router-view />
     </div>
@@ -10,27 +9,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
-import { useStore } from '@/store';
+import { defineComponent } from 'vue';
 import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
-import ChatBox from './components/ChatBox.vue';
 
 export default defineComponent({
-  components: { Navbar, Footer, ChatBox },
-  setup() {
-    const store = useStore();
-    // const users = ref<User[]>();
-    // const getUsers = () => {
-    //   api.users.getUsers()
-    //     .then((response) => {
-    //       users.value = response.data;
-    //     });
-    // };
-    // getUsers();
-    return {
-      currentUser: computed(() => store.getUser),
-    };
+  components: {
+    Navbar,
+    Footer,
   },
 });
 </script>
