d625e870690327e6c52b9aefd298a66c9416dea1
ppaglier <ppaglier@student.42.fr>
2022-02-21 01:32:11 +0100

    change main (config to swaggerConfig)

--- backend/app/src/main.ts
+++ backend/app/src/main.ts
@@ -6,13 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: 'http://localhost:8080', credentials: true });
 
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Backend API Docs')
     .setDescription('The backend of ft_transcendence')
     .setVersion('1.0')
     .build();
-
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000, '0.0.0.0');
