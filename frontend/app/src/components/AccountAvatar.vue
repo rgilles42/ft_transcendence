8a72acc0afb6329f97357da267967a1f15365d0b
ppaglier <ppaglier@student.42.fr>
2022-02-25 00:00:19 +0100

    better accountAvatar component

--- frontend/app/src/components/AccountAvatar.vue
+++ frontend/app/src/components/AccountAvatar.vue
@@ -1,30 +1,42 @@
 <template>
   <img
     v-if="user"
-    :src="avatarUrl"
-    @err="$emit('err', $event)"
+    :src="avatarSrc"
+    @err="emit('err', $event)"
     />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed } from 'vue';
 import User from '@/types/User';
+// import api from '@/api';
+import { configService } from '@/services/configService';
 
 export default defineComponent({
   name: 'AccountAvatar',
   props: {
     user: Object as () => User,
   },
-  computed: {
-    avatarUrl() {
-      if (!this.user) {
+  setup(props, { emit }) {
+    const getAvatarSrc = () => {
+      if (!props.user) {
         return 'https://eu.ui-avatars.com/api/?name=John+Doe&background=random';
       }
-      if (!this.user.imageUrl) {
-        return `https://eu.ui-avatars.com/api/?name=${this.user.username}&background=random`;
+      if (!props.user.imageUrl) {
+        return `https://eu.ui-avatars.com/api/?name=${props.user.username}&background=random`;
       }
-      return (this.user.imageUrl);
-    },
+      if (props.user.imageUrl.startsWith('http')) {
+        return (props.user.imageUrl);
+      }
+      return `${configService.getApiUrl()}/storage/${props.user.imageUrl}`;
+    };
+
+    const avatarSrc = computed(() => getAvatarSrc());
+
+    return {
+      avatarSrc,
+      emit,
+    };
   },
 });
 </script>
