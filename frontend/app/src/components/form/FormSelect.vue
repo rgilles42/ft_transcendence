0ca6d1aa65ec122d1cdb3387f902f4008f320e23
ppaglier <ppaglier@student.42.fr>
2022-02-27 21:26:22 +0100

    add FormSelect + better channel creation form

---
+++ frontend/app/src/components/form/FormSelect.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="form-textarea">
+    <slot name="label" v-bind="{ title }">
+    <label :title="title" :for="id">{{ title }}</label>
+    </slot>
+    <slot name="errors" v-bind="{ getErrors }">
+      <span v-if="getErrors.length > 0" class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+        <li v-for="(error, index) in getErrors" :key="index">{{ error }}</li>
+      </span>
+    </slot>
+    <slot name="input" v-bind="{ getErrors, currentValue, required }">
+      <select :id="id" :name="name" v-model="currentValue" class="mt-0" :class="[getErrors.length > 0 ? 'border-red-500 border' : '']" :required="required" :disabled="disabled" :readonly="readonly">
+        <option v-for="(option, index) in options" :key="index" :value="option">
+          <slot name="option" v-bind="{ getErrors, option, isSelected: (currentValue === option), index, currentValue, required }">
+            {{ option }}
+          </slot>
+        </option>
+      </select>
+    </slot>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent, ref, computed, watch,
+} from 'vue';
+
+export default defineComponent({
+  name: 'FormSelect',
+  props: {
+    title: String,
+    name: String,
+    id: String,
+    modelValue: null,
+    errors: Object,
+    required: Boolean,
+    disabled: Boolean,
+    readonly: Boolean,
+    options: Array as unknown as Array<any>,
+  },
+  setup(props, { emit }) {
+    const currentValue = ref(props.modelValue);
+
+    const getCurrentErrors = () => {
+      const errors: string[] = [];
+      if (!props.errors || !props.name || !props.errors[props.name]) {
+        return errors;
+      }
+      return props.errors[props.name];
+    };
+
+    const getErrors = computed(() => getCurrentErrors());
+
+    watch(
+      () => currentValue.value,
+      () => emit('update:modelValue', currentValue.value),
+      { immediate: true },
+    );
+
+    return {
+      currentValue,
+      getErrors,
+    };
+  },
+});
+</script>
--- frontend/app/src/views/chat/create.vue
+++ frontend/app/src/views/chat/create.vue
@@ -7,10 +7,10 @@
         <div class="mb-4 p-4 bg-gray-700 bg-opacity-50 rounded flex-1">
 
           <div class="px-6 py-4 grid md:grid-flow-col gap-4">
-            <FormInput title="Nom du salon" name="title" id="title" v-model="newChannelData.title" :errors="errors" :required="true" />
-            <FormInput title="Activer le mot de passe" type="checkbox" v-model="newChannelData.isPasswordEnabled" :errors="errors" />
-            <FormInput title="Mot de passe" type="password" name="password" id="password" v-model="newChannelData.password" :errors="errors" :disabled="!newChannelData.isPasswordEnabled" />
-            <FormInput :title="`Type: ${newChannelData.isPrivate ? 'Privée' : 'Public'}`" type="checkbox" name="isPrivate" id="isPrivate" v-model="newChannelData.isPrivate" :errors="errors" />
+            <FormInput title="Nom du salon" name="title" v-model="newChannelData.title" :errors="errors" :required="true" />
+            <FormSelect title="Type" name="isPrivate" v-model="newChannelData.channelType" :options="channelTypes" :errors="errors" required />
+            <FormInput title="Activer le mot de passe" type="checkbox" v-model="newChannelData.isPasswordEnabled" :errors="errors" :disabled="newChannelData.isPrivate" />
+            <FormInput title="Mot de passe" type="password" name="password" v-model="newChannelData.password" :errors="errors" :disabled="!newChannelData.isPasswordEnabled || newChannelData.isPrivate" />
           </div>
 
           <span v-if="errors.global && errors.global.length > 0" class="flex items-center font-medium tracking-wide text-red-500 text-xs ml-1">
@@ -29,23 +29,37 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
 import api from '@/api';
 import Channel, { DEFAULT_CHANNEL } from '@/types/Channel';
 import { useRouter } from 'vue-router';
+import FormSelect from '@/components/form/FormSelect.vue';
 import FormInput from '../../components/form/FormInput.vue';
 
 export default defineComponent({
   name: 'ChatCreation',
-  components: { FormInput },
+  components: { FormInput, FormSelect },
   setup() {
     const router = useRouter();
 
-    const newChannelData = ref<Channel>({ ...DEFAULT_CHANNEL, isPasswordEnabled: false });
+    const channelTypes = [
+      'Public',
+      'Privé',
+    ];
+
+    const newChannelData = ref<Channel>({ ...DEFAULT_CHANNEL, channelType: channelTypes[0], isPasswordEnabled: false });
     const errors = ref<any>({});
 
+    watch(
+      () => newChannelData.value.channelType,
+      () => {
+        newChannelData.value.isPrivate = channelTypes[0] !== newChannelData.value.channelType;
+      },
+      { immediate: true },
+    );
+
     const checkForm = () => {
-      if (!newChannelData.value.isPasswordEnabled) {
+      if (!newChannelData.value.isPasswordEnabled || newChannelData.value.isPrivate) {
         newChannelData.value.password = null;
       }
       api.channels.createChannel(newChannelData.value)
@@ -61,6 +75,7 @@ export default defineComponent({
         });
     };
     return {
+      channelTypes,
       newChannelData,
       checkForm,
       errors,
